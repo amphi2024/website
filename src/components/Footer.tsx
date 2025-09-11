e509d6a000ae494bd757eafb5d07e549eb08e789
Siar <siar019283@gmail.com>
2025-09-11 22:54:41 +0900

    feat: add twitter, youtube button

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,19 +1,13 @@
 import {Link} from "react-router-dom";
 import {useTranslation} from "react-i18next";
 import styled from "styled-components";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faGithub, faXTwitter, faYoutube} from "@fortawesome/free-brands-svg-icons";
 
-function Footer() {
-
-    const {t} = useTranslation();
-
-    const AppFooter = styled.footer`
+const AppFooter = styled.footer`
         text-align: center;
         padding: 20px;
         background-color: var(--footer-background-color);
-
-        //* {
-        //    margin: var(--item-margin);
-        //}
 
         a {
             font-size: 15px;
@@ -22,10 +16,29 @@ function Footer() {
             margin: var(--item-margin);
         }
         
+        svg {
+            font-size: 30px;
+        }
+        
         div{
             margin-top: 10px;
         }
     `;
+
+const FooterButton = styled.button`
+    background-color: transparent;
+    padding: 8px;
+    font-size: 15px;
+    color: var(--text-color-secondary);
+    
+    &:hover {
+        color: var(--text-color);
+    }
+`
+
+function Footer() {
+
+    const {t} = useTranslation();
 
     return (
         <AppFooter className="AppFooter">
@@ -39,9 +52,23 @@ function Footer() {
                 <Link to={"/terms"}>
                     {t("termsAndCondition")}
                 </Link>
-                <Link to={"https://github.com/amphi2024"}>
-                    {t("github")}
-                </Link>
+            </div>
+            <div>
+                <FooterButton onClick={() => {
+                    window.open("https://github.com/amphi2024", "_blank");
+                }}>
+                    <FontAwesomeIcon icon={faGithub} />
+                </FooterButton>
+                <FooterButton onClick={() => {
+                    window.open("https://x.com/amphi2024", "_blank");
+                }}>
+                    <FontAwesomeIcon icon={faXTwitter}/>
+                </FooterButton>
+                <FooterButton onClick={() => {
+                    window.open("https://youtube.com/@amphi2024", "_blank");
+                }}>
+                    <FontAwesomeIcon icon={faYoutube}/>
+                </FooterButton>
             </div>
         </AppFooter>
     );
