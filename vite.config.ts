c9c93d9a4f81f858ded2c906e2875bed140917c1
siar1234 <siar019283@gmail.com>
2026-03-20 09:24:10 +0900

    feat: add robots.txt and sitemap

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import Sitemap from 'vite-plugin-sitemap';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+      react(),
+    Sitemap({
+      hostname: 'https://amphi.site',
+      dynamicRoutes: ["/", '/notes', '/music', "/photos", "/cloud", "/server", "/pricing", "/privacy", "/terms", "/archive/privacy", "/archive/terms"]
+    }),
+  ],
 })
